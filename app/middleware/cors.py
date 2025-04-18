2b7ed92ba57822633e68198fe52158b0e5e9335d
宫贺 <he.gong@shmedo.cn>
2025-04-18 23:34:12 +0800

    Update CORS middleware to specify allowed origins for enhanced security

--- app/middleware/cors.py
+++ app/middleware/cors.py
@@ -6,9 +6,14 @@ def setup_cors_middleware(app: FastAPI) -> None:
     """
     设置CORS中间件
     """
+
+    origins = [
+        "https://app-remote-config.lumberjack2.com",
+        "http://localhost:3000",
+    ]
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  # 允许所有来源，生产环境中应该设置为特定的来源
+        allow_origins=origins,  # 允许
         allow_credentials=True,
         allow_methods=["*"],  # 允许所有方法
         allow_headers=["*"],  # 允许所有头
